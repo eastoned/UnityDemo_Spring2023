bed0a741533b3e89f0893b23f56b7ae6abab2ed8
Easton <eastonself@gmail.com>
2023-02-15 17:56:22 -0500

    interaction demo

--- Assets/E3_DATANARRATIVE/Player.cs
+++ Assets/E3_DATANARRATIVE/Player.cs
@@ -16,11 +16,6 @@ public class Player : MonoBehaviour
     public Vector3 Direction;
     public float timer;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -33,13 +28,6 @@ public class Player : MonoBehaviour
         timer += Time.deltaTime * speed;
         transform.position = amount*Vector3.up * Mathf.Sin(timer);
         
-    }
-
-    bool IsCubeEnabled(){
-        //if cube blue then
-        return true;
-        //else
-        return false;
     }
     
 }
---
+++ Assets/E4_INTERACTIVEDIORAMA/MouseDragInput.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MouseDragInput : MonoBehaviour
+{
+    void Start(){
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+    public float GetAxisCustom(string axisName){
+        if(axisName == "Mouse X"){
+            if (Input.GetMouseButton(0)){
+                return UnityEngine.Input.GetAxis("Mouse X");
+            } else{
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y"){
+            if (Input.GetMouseButton(0)){
+                return UnityEngine.Input.GetAxis("Mouse Y");
+            } else{
+                return 0;
+            }
+        }
+        return UnityEngine.Input.GetAxis(axisName);
+    }
+}
